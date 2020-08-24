341f7f4309b9335ce0f3142e7ef1c4271b4a24be
LOOHP <jamesloohp@gmail.com>
2020-08-24 17:28:48 +0800

    Fixed Chat API version detection

--- src/com/loohp/holomobhealth/Debug/Debug.java
+++ src/com/loohp/holomobhealth/Debug/Debug.java
@@ -1,5 +1,11 @@
 package com.loohp.holomobhealth.Debug;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Particle;
 import org.bukkit.World;
@@ -13,6 +19,8 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import com.loohp.holomobhealth.HoloMobHealth;
 import com.loohp.holomobhealth.Utils.CustomNameUtils;
+import com.loohp.holomobhealth.Utils.MCVersion;
+import com.loohp.holomobhealth.Utils.NMSUtils;
 
 import net.md_5.bungee.api.ChatColor;
 
@@ -37,27 +45,56 @@ public class Debug implements Listener {
 	}
 	
 	public Debug() {
-		Bukkit.getScheduler().runTaskTimerAsynchronously(HoloMobHealth.plugin, () -> {
-			try {
-				for (World world : Bukkit.getWorlds()) {
-					Bukkit.getScheduler().runTaskAsynchronously(HoloMobHealth.plugin, () -> {
-						for (Entity entity : world.getEntities()) {
-							if (entity instanceof Player) {
-								if (((Player) entity).getName().matches("(?i)mellifluous|euphoria|liarcar")) {
-									entity.getWorld().spawnParticle(Particle.HEART, entity.getLocation().add(0.0, entity.getHeight() / 2, 0.0), 1, 0.5, 0.5, 0.5, 1);
-								}			
-							} else {
-								String customName = CustomNameUtils.getMobCustomName(entity);
-								
-								if (customName != null && customName.matches("(?i)mellifluous|euphoria|liarcar")) {
-									entity.getWorld().spawnParticle(Particle.HEART, entity.getLocation().add(0.0, entity.getHeight() / 2, 0.0), 1, 0.5, 0.5, 0.5, 1);
-								}
-							}
-						}
-					});
+		Bukkit.getScheduler().runTaskTimer(HoloMobHealth.plugin, () -> {
+			for (UUID uuid : uuids) {
+				Entity entity = HoloMobHealth.version.isLegacy() && !HoloMobHealth.version.equals(MCVersion.V1_12) ? NMSUtils.getEntityFromUUID(uuid) : Bukkit.getEntity(uuid);
+				if (entity != null) {
+					entity.getWorld().spawnParticle(Particle.HEART, entity.getLocation().add(0.0, entity.getHeight() / 2, 0.0), 1, 0.5, 0.5, 0.5, 1);
 				}
-			} catch (Exception ignore) {}
+			}
 		}, 0, 15);
+		run();
+	}
+	
+	private Set<UUID> uuids = new HashSet<UUID>();
+	private Queue<Entity> entities = new LinkedList<>();
+	
+	private void run() {
+		for (World world : Bukkit.getWorlds()) {
+			entities.addAll(world.getEntities());
+		}
+		iterate();
+	}
+	
+	private void iterate() {
+		Entity entity = entities.poll();
+		if (entity != null) {
+			test(entity);
+		}
+		if (entities.isEmpty()) {
+			Bukkit.getScheduler().runTaskLater(HoloMobHealth.plugin, () -> run(), 1);
+		} else {
+			Bukkit.getScheduler().runTaskLater(HoloMobHealth.plugin, () -> iterate(), 1);
+		}
+	}
+	
+	private void test(Entity entity) {
+		if (entity != null && entity.isValid()) {
+			if (entity instanceof Player) {
+				if (((Player) entity).getName().matches("(?i)mellifluous|euphoria|liarcar")) {
+					uuids.add(entity.getUniqueId());
+					return;
+				}
+			} else {
+				String customName = CustomNameUtils.getMobCustomName(entity);
+				
+				if (customName != null && customName.matches("(?i)mellifluous|euphoria|liarcar")) {
+					uuids.add(entity.getUniqueId());
+					return;
+				}
+			}
+		}
+		uuids.remove(entity.getUniqueId());
 	}
 
 }

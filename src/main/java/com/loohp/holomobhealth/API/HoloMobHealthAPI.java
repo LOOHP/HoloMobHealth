c47119a081bfadb2dedd4593fa0c655a913d6483
LOOHP <jamesloohp@gmail.com>
2021-03-27 17:37:38 +0800

    API documentation

--- src/main/java/com/loohp/holomobhealth/API/HoloMobHealthAPI.java
+++ src/main/java/com/loohp/holomobhealth/API/HoloMobHealthAPI.java
@@ -4,14 +4,29 @@ import com.loohp.holomobhealth.Registries.CustomPlaceholderScripts;
 
 public class HoloMobHealthAPI {
 	
+	/**
+	 * Register a class to be used in custom placeholder scripts
+	 * @param The class name
+	 * @param The class
+	 */
 	public static void registerClassToCustomPlaceholderScript(String identifier, Class<?> clazz) {
 		CustomPlaceholderScripts.registerClass(identifier, clazz);
 	}
 	
+	/**
+	 * Check if a class has already be registered to be used in custom placeholder scripts
+	 * @param The class name
+	 * @return TRUE/FALSE
+	 */
 	public static boolean containsClassInCustomPlaceholderScript(String identifier) {
 		return CustomPlaceholderScripts.containsClass(identifier);
 	}
 	
+	/**
+	 * Check if a class has already be registered to be used in custom placeholder scripts
+	 * @param The class
+	 * @return TRUE/FALSE
+	 */
 	public static boolean containsClassInCustomPlaceholderScript(Class<?> clazz) {
 		return CustomPlaceholderScripts.containsClass(clazz);
 	}

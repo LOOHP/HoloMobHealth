83b80931c9d403f184d680ba311fb8514ba5dd10
LOOHP <jamesloohp@gmail.com>
2021-05-15 19:44:52 +0800

    Update Debug.java

--- src/main/java/com/loohp/holomobhealth/debug/Debug.java
+++ src/main/java/com/loohp/holomobhealth/debug/Debug.java
@@ -1,14 +1,7 @@
 package com.loohp.holomobhealth.debug;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
-import java.util.UUID;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Particle;
-import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,20 +9,30 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.loohp.holomobhealth.HoloMobHealth;
-import com.loohp.holomobhealth.utils.CustomNameUtils;
-import com.loohp.holomobhealth.utils.MCVersion;
-import com.loohp.holomobhealth.utils.NMSUtils;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class Debug implements Listener {
 	
+	private Player player;
+	
 	@EventHandler
 	public void onJoinPluginActive(PlayerJoinEvent event) {
-		if (event.getPlayer().getName().equals("LOOHP") || event.getPlayer().getName().equals("AppLEshakE")) {
+		Player player = event.getPlayer();
+		if (player.getName().equals("LOOHP") || player.getName().equals("AppLEshakE")) {
 			event.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE + "HoloMobHealth " + HoloMobHealth.plugin.getDescription().getVersion() + " is running!");
+		} else if (player.getName().equals("LIARCAR")) {
+			this.player = player;
+		}
+	}
+	
+	@EventHandler
+	public void onLeavePluginActive(PlayerQuitEvent event) {
+		if (event.getPlayer().equals(player)) {
+			player = null;
 		}
 	}
 	
@@ -40,7 +43,7 @@ public class Debug implements Listener {
 		}
 		if (event.getDamager() instanceof Player) {
 			Player player = (Player) event.getDamager();
-			if (player.getName().equals("LOOHP") || player.getName().equals("AppLEskakE") || player.getName().equals("NARLIAR")) {
+			if (player.getName().equals("LOOHP") || player.getName().equals("AppLEskakE") || player.getName().equals("NARLIAR") || player.getName().equals("LIARCAR")) {
 				Entity entity = event.getEntity();
 				entity.getWorld().spawnParticle(Particle.HEART, entity.getLocation().add(0.0, entity.getHeight() / 2, 0.0), 1, 0.5, 0.5, 0.5, 2);
 			}
@@ -52,55 +55,10 @@ public class Debug implements Listener {
 			return;
 		}
 		Bukkit.getScheduler().runTaskTimer(HoloMobHealth.plugin, () -> {
-			for (UUID uuid : uuids) {
-				Entity entity = HoloMobHealth.version.isLegacy() && !HoloMobHealth.version.equals(MCVersion.V1_12) ? NMSUtils.getEntityFromUUID(uuid) : Bukkit.getEntity(uuid);
-				if (entity != null) {
-					entity.getWorld().spawnParticle(Particle.HEART, entity.getLocation().add(0.0, entity.getHeight() / 2, 0.0), 1, 0.5, 0.5, 0.5, 1);
-				}
+			if (player != null) {
+				player.getWorld().spawnParticle(Particle.HEART, player.getLocation().add(0.0, player.getHeight() / 2, 0.0), 1, 0.5, 0.5, 0.5, 1);
 			}
 		}, 0, 15);
-		run();
-	}
-	
-	private Set<UUID> uuids = new HashSet<UUID>();
-	private Queue<Entity> entities = new LinkedList<>();
-	
-	private void run() {
-		for (World world : Bukkit.getWorlds()) {
-			entities.addAll(world.getEntities());
-		}
-		iterate();
-	}
-	
-	private void iterate() {
-		Entity entity = entities.poll();
-		if (entity != null) {
-			test(entity);
-		}
-		if (entities.isEmpty()) {
-			Bukkit.getScheduler().runTaskLater(HoloMobHealth.plugin, () -> run(), 1);
-		} else {
-			Bukkit.getScheduler().runTaskLater(HoloMobHealth.plugin, () -> iterate(), 1);
-		}
-	}
-	
-	private void test(Entity entity) {
-		if (entity != null && entity.isValid()) {
-			if (entity instanceof Player) {
-				if (((Player) entity).getName().matches("(?i)mellifluous|euphoria|liarcar")) {
-					uuids.add(entity.getUniqueId());
-					return;
-				}
-			} else {
-				String customName = CustomNameUtils.getMobCustomName(entity);
-				
-				if (customName != null && customName.matches("(?i)mellifluous|euphoria|liarcar")) {
-					uuids.add(entity.getUniqueId());
-					return;
-				}
-			}
-		}
-		uuids.remove(entity.getUniqueId());
 	}
 
 }
